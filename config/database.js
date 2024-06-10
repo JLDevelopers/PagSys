2b55bc709570b78a876a965ba24b7a3d4d0c0853
JLDevelopers <163607601+JLDevelopers@users.noreply.github.com>
2024-06-09 22:30:47 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,1 +1,9 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('pagsys', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+module.exports = sequelize;
 
